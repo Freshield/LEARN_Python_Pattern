698a3d4037cfb2579bc43878c568bb46277bb142
freshield <zxdsw199182@gmail.com>
2019-08-31 12:03:20 +0800

    done singeton

--- chp2_review/a1_singleton.py
+++ chp2_review/a1_singleton.py
@@ -14,3 +14,19 @@
 @                                    Freshield @
 @==============================================@
 """
+
+
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+
+        return cls.instance
+
+
+if __name__ == '__main__':
+    s = Singleton()
+    print('object created', s)
+
+    s1 = Singleton()
+    print('object created', s1)
