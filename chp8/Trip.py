9b29546f0ff6f16c4d47cd9419869ca69904cf6f
freshield <zxdsw199182@gmail.com>
2019-10-13 13:24:11 +0800

    continue

---
+++ chp8/Trip.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: Trip.py
+@Time: 2019-10-12 11:34
+@Last_update: 2019-10-12 11:34
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class Trip(metaclass=ABCMeta):
+
+    @abstractmethod
+    def setTransport(self):
+        pass
+
+    @abstractmethod
+    def day1(self):
+        pass
+
+    @abstractmethod
+    def day2(self):
+        pass
+
+    @abstractmethod
+    def day3(self):
+        pass
+
+    @abstractmethod
+    def returnHome(self):
+        pass
+
+    def itinerary(self):
+        self.setTransport()
+        self.day1()
+        self.day2()
+        self.day3()
+        self.returnHome()
