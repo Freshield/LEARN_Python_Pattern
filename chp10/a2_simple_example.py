856403377709308907eb6b0061c979c1344c0202
freshield <zxdsw199182@gmail.com>
2019-10-23 18:36:53 +0800

    begin the simple example

---
+++ chp10/a2_simple_example.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a2_simple_example.py
+@Time: 2020-03-08 18:26
+@Last_update: 2020-03-08 18:26
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
