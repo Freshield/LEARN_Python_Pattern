4c30d7a66075d034a95f9c8361409d7d2ca84dce
freshield <zxdsw199182@gmail.com>
2020-01-24 11:33:06 +0800

    try inherent

---
+++ chp1_review/b3_inherent.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: b3_inherent.py
+@Time: 2020-01-25 11:32
+@Last_update: 2020-01-25 11:32
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+
+
+class A:
+    def a1(self):
+        print('a1')
+
+class B(A):
+    def b(self):
+        print('b')
+
+
+if __name__ == '__main__':
+    b = B()
+    b.a1()
